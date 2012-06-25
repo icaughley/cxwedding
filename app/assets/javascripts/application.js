616f4c8217909664c7149c6912a2b2adcec7c8b1
unknown <Ian@ANTONOV.(none)>
2012-06-26 08:05:28 +1000

    Fixed problem with boostrap. Shouldn't use the bootstrap.min.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap/bootstrap.min
 //= require bootstrap/bootstrap-alert
 //= require bootstrap/bootstrap-button
 //= require bootstrap/bootstrap-collapse
