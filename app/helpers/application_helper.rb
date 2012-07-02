dda95c4f429e579228860298d3f13d9ee8dd6ed5
unknown <Ian@ANTONOV.(none)>
2012-07-03 09:30:47 +1000

    Added proper Ajax js views for create, edit and delete actions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def update_flash_messages
+    raw "$('#flash_messages').html(' #{escape_javascript( render 'layouts/messages' )}');"
+  end
+
+  def hide_modal
+    raw "$('#modal').modal('hide');"
+  end
+
 end
